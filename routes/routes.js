1860f1fd5e9c73ad42aff93cff31fe80f7ca2b73
kenshinf <kenshin_fetalvero@dlsu.edu.ph>
2024-04-04 17:14:37 +0800

    Add delete page

--- routes/routes.js
+++ routes/routes.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/controller.js');
 app.get('/', controller.getIndex);
 app.get('/create', controller.getCreate);
 app.get('/view', controller.getView);
+app.get('/delete', controller.getDelete);
 
 
 //Error
