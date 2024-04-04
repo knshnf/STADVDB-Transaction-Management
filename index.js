a6f57bb36ba60d98dec92fbf25328272c8ad46cb
kenshinf <kenshin_fetalvero@dlsu.edu.ph>
2024-04-04 17:54:31 +0800

    Create MySQL connection

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/routes.js');
 const hbs = require(`hbs`);
 const app = express();
+const mysql = require('mysql');
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
@@ -14,6 +15,22 @@ app.use('/', routes);
 dotenv.config();
 port = process.env.PORT;
 hostname = process.env.HOSTNAME;
+dbhost = process.env.MYSQLHOST;
+dbport = process.env.MYSQLPORT;
+dbuser = process.env.MYSQLUSER;
+dbpass = process.env.MYSQLPASS;
+
+var con = mysql.createConnection({
+    host: dbhost,
+    user: dbuser,
+    password: dbpass,
+    port: dbport
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected to MySQL!");
+});
 
 app.listen(port, function() {
     console.log('Server running at: ' + port);
